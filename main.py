245efe9aff622ec2a2e45699fb1484e1d85156ee
bakertj3 <thetrevor@gmail.com>
2026-01-03 12:33:44 -0500

    player ship displays & rotates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import constants
 from logger import log_state
+import player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,16 +14,18 @@ def main():
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    player_ship = player.Player((constants.SCREEN_WIDTH / 2), (constants.SCREEN_HEIGHT / 2))
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_ship.update(dt)
+        player_ship.draw(screen)
         pygame.display.flip()
-        clocktime.tick(60)
-        dt = clocktime.tick() / 1000
-        print(dt)
+        dt = clocktime.tick(60) / 1000
 
 
 
---
+++ player.py
@@ -0,0 +1,32 @@
+from circleshape import CircleShape
+import constants
+import pygame
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        super().__init__(x, y, constants.PLAYER_RADIUS)
+        self.rotation = 0
+    
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        return self.rotation + constants.PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotation = self.rotate(-1 * dt)
+        if keys[pygame.K_d]:
+            self.rotation = self.rotate(dt)
